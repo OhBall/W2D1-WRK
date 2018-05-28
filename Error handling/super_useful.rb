03c7cecd8401379800d540ad4cf714b6c8477a64
Aaron Oliver <olivermartinball@gmail.com>
2018-05-28 13:53:56 -0400

    Raise proper errors.

--- Error handling/super_useful.rb
+++ Error handling/super_useful.rb
@@ -1,31 +1,47 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError => error
+    return nil
+  end
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
-
+class CoffeeError < StandardError ; end
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
+    if maybe_fruit == "coffee"
+      raise CoffeeError
+    end
     raise StandardError 
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => error
+    puts "Coffee will do for now..."
+    retry
+  end
 end  
 
 # PHASE 4
+class NotEnoughYears < StandardError;end
+class EmptyInput < StandardError;end
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
+    raise NotEnoughYears if yrs_known < 5
+    raise EmptyInput if name.empty? || fav_pastime.empty?
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
   end
@@ -42,5 +58,4 @@ class BestFriend
     puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
   end
 end
-
 
