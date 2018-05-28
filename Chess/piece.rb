34cbe74cac997e2541fd1b3a01c01073cd2d3db0
Aaron Oliver <olivermartinball@gmail.com>
2018-05-28 17:32:23 -0400

    Begin Implementing Sliding Pieces

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -6,8 +6,36 @@ class Piece
     @pos = pos 
   end 
   
+  def moves 
+    return []
+  end
+  
   def to_s
     return "P"
   end
   
 end 
+
+class Bishop < Piece
+  include SlidingPiece
+  
+  def move_dirs
+    :diagonal
+  end
+end
+
+class Rook < Piece
+  include SlidingPiece
+  
+  def move_dirs
+    :straight
+  end
+end
+
+class Queen < Piece
+  include SlidingPiece
+  
+  def move_dirs
+    :both
+  end
+end
---
+++ Chess/sliding_piece.rb
@@ -0,0 +1,15 @@
+module SlidingPiece
+  def moves
+    possible_moves = []
+    case move_dirs
+    when :diagonal
+      
+    when :straight
+      diff = 0 
+      y, x = self.pos 
+      while self.board[]
+    when :both
+      
+    end
+  end
+end
